2bff8e41b1b38368a79801264af2244bc95785da
bmsurilla <bm.surilla@gmail.com>
2026-05-25 23:08:08 +0200

    continued with split

--- ft_split.c
+++ ft_split.c
@@ -12,7 +12,46 @@
 
 #include "libft.h"
 
+static int  ft_wordcount(char *str, char c)
+{
+    int i;
+    int word;
+
+    i = 0;
+    word = 0;
+    while (str[i] != 0)
+    {
+        if (str[i] != c && ((i == 0) || (str[i - 1] == c)))
+        {
+            word++;
+        }
+        i++;
+    }
+    return (word);
+}
+
+static size_t   ft_wordlen(char *str, char c)
+{
+    int i;
+
+    while (word[i] != 0)
+    {
+
+
+    }
+}
+
+
 char	**ft_split(char const *s, char c)
 {
-	
+    int     i;
+    int     word;
+    char    **splitwords;
+    int     wordlen;
+
+    word = ft_wordcount(s,c);
+    splitwords = malloc (word + 1);
+    if (!splitwords)
+        return (NULL);
+
 }
